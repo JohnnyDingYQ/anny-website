69435e5a6d900fbf7e742e0760da76f7d34b394b
JohnnyDingYQ <johnnydyq@outlook.com>
2025-06-21 16:28:21 +0800

    added interactive map

--- app/app.py
+++ app/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.jinja")
 
+@app.route("/tcm/interactive-map", methods=["GET"])
+def interactive_map():
+    return render_template("interactive-map.jinja")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
